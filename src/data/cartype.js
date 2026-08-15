f2177bdcc1b7cef7f997b816407113337a6c6549
Notfound-404 <jeezstok2@gmail.com>
2026-08-16 00:31:15 +0800

    update harga

--- src/data/cartype.js
+++ src/data/cartype.js
@@ -11,29 +11,29 @@ export const carTypes = [
   {
     id: "premium",
     name: "Premium Car",
-    price: "50.000",
+    price: "300.000/16,82 USD",
     maxPassengers: 7,
     models: [
-      { id: 'innova', name: 'Toyota Innova Reborn', price: '50.000', image: 'https://th.bing.com/th/id/OIP.G0CU2Jo5cPr7Zm9Loc5fdAHaEZ?w=229&h=150&c=6&r=0&o=7&dpr=1.3&pid=1.7&rm=3' }
+      { id: 'innova', name: 'Toyota Innova Reborn', price: '300.000/16,82 USD', image: 'https://th.bing.com/th/id/OIP.G0CU2Jo5cPr7Zm9Loc5fdAHaEZ?w=229&h=150&c=6&r=0&o=7&dpr=1.3&pid=1.7&rm=3' }
     ]
   },
   {
     id: "luxury",
     name: "Luxury Car",
-    price: "100.000",
+    price: "1.500.000-1.700.000/84,12-95,46 USD",
     maxPassengers: 5,
     models: [
-      { id: 'alphard', name: 'Toyota Alphard', price: '200.000', image: 'https://global.toyota/pages/news/images/2023/06/21/1330/rendition/20230621_01_12_W480_H320.jpg' },
-      { id: 'denza', name: 'Toyota Denza', price: '250.000', image: 'https://denza.harmonyauto.sg/wp-content/uploads/2024/10/design-img-1.jpg' }
+      { id: 'alphard', name: 'Toyota Alphard', price: '1.500.000/84,12 USD', image: 'https://global.toyota/pages/news/images/2023/06/21/1330/rendition/20230621_01_12_W480_H320.jpg' },
+      { id: 'denza', name: 'Toyota Denza', price: '1.700.000/95,46 USD', image: 'https://denza.harmonyauto.sg/wp-content/uploads/2024/10/design-img-1.jpg' }
     ]
   },
   { 
     id: "minibus",
     name: "Minibus",
-    price: "150.000",
+    price: "800.000/44,44 USD",
     maxPassengers: 12,
     models: [
-        { id: 'hiace', name: 'Toyota Hiace', price: '150.000', image: 'https://th.bing.com/th/id/OIP.yOZV9WRYbVMVZsjcHheYygHaEK?w=284&h=180&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3' },
+        { id: 'hiace', name: 'Toyota Hiace', price: '800.000/44,44 USD', image: 'https://th.bing.com/th/id/OIP.yOZV9WRYbVMVZsjcHheYygHaEK?w=284&h=180&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3' },
     ]
   }
 ];
